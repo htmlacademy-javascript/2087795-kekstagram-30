4b84fb1f5a8bcc6511846250b0db739faf9e51e6
TashaTul <natu.tulyakova@gmail.com>
2023-10-30 16:28:50 +0300

    Вносит правки

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,11 +1,11 @@
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
 
-const createThumbnaail = ({url, description, comments, likes}) => {
+const createThumbnail = ({url, description, comments, likes}) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
-
-  thumbnail.querySelector('.picture__img').src = url;
-  thumbnail.querySelector('.picture__img').alt = description;
+  const imagePicture = thumbnail.querySelector('.picture__img');
+  imagePicture.src = url;
+  imagePicture.alt = description;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.querySelector('.picture__likes').textContent = likes;
 
@@ -15,7 +15,7 @@ const createThumbnaail = ({url, description, comments, likes}) => {
 const renderThumbnails = (pictures) => {
   const fragment = document.createDocumentFragment();
   pictures.forEach((picture) => {
-    const thumbnail = createThumbnaail(picture);
+    const thumbnail = createThumbnail(picture);
     fragment.append(thumbnail);
   });
 
