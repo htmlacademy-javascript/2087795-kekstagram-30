6256b894952a8847b605d32bf62c073d22ccf6d4
TashaTul <natu.tulyakova@gmail.com>
2023-11-06 11:06:02 +0300

    Добавляет комментарии

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -1,43 +1,19 @@
-const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const container = document.querySelector('.pictures');
-//Переменные для открытия и закрытия попапа
-const bigPicture = document.querySelector('.big-picture');
-const body = document.querySelector('body');
-const commentNot = document.querySelector('.social__comment-count');
-const commentLoaderNot = document.querySelector('.social__comments-loader');
+const thumbnailTemplate = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
 
-const createThumbnail = ({url, description, comments, likes}) => {
+const createThumbnail = ({url, description, comments, likes, id}) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
   const imagePicture = thumbnail.querySelector('.picture__img');
   imagePicture.src = url;
   imagePicture.alt = description;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.querySelector('.picture__likes').textContent = likes;
-  //Добавила обработчик событий на клик, появляется модальное окно
-  thumbnail.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    bigPicture.classList.remove('hidden');
-    body.classList.add('modal-open');
-    commentNot.classList.add('hidden');
-    commentLoaderNot.classList.add('hidden');
-  });
-  //Конец тут
+  thumbnail.dataset.thumbnailId = id;
   return thumbnail;
 };
 
-//Закрывает попап по кнопке
-const closeBigPicture = document.querySelector('#picture-cancel');
-closeBigPicture.addEventListener('click', () => {
-  bigPicture.classList.add('hidden');
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.keyCode === 27) {
-    bigPicture.classList.add('hidden');
-  }
-});
-
-const renderThumbnails = (pictures) => {
+const renderThumbnails = (pictures, container) => {
   const fragment = document.createDocumentFragment();
   pictures.forEach((picture) => {
     const thumbnail = createThumbnail(picture);
