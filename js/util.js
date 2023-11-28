500eeb96f744dcd789a4560f062184c04707e839
TashaTul <natu.tulyakova@gmail.com>
2023-11-28 14:11:24 +0300

    Вносит правки для защиты

--- js/util.js
+++ js/util.js
@@ -1,14 +1,4 @@
-// Функция генерации случайных числел в заданном диапозоне с двумя параметрами где (а) минимальное число, а (b) максимальное число
-// const getRandomInteger = (a, b) => {
-//   const lower = Math.ceil(Math.min(a, b));
-//   const upper = Math.floor(Math.max(a, b));
-//   const result = Math.random() * (upper - lower + 1) + lower;
-//   return Math.floor(result);
-// };
-
-// export {getRandomInteger};
-
-////
+const REMOVE_MESSAGE_TIMEOUT = 5000;
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
@@ -16,8 +6,6 @@ const errorMessageTemplate = document
   .querySelector('#data-error')
   .content
   .querySelector('.data-error');
-
-const REMOVE_MESSAGE_TIMEOUT = 5000;
 
 const showErrorMessage = () => {
   const errorElement = errorMessageTemplate.cloneNode(true);
@@ -37,43 +25,3 @@ const debounce = (callback, timeoutDelay = 500) => {
 };
 
 export { isEscapeKey, showErrorMessage, debounce };
-
-/////
-
-// const isEscapeKey = (evt) => evt.key === 'Escape';
-
-// const errorMessageTemplate = document
-//   .querySelector('#data-error')
-//   .content
-//   .querySelector('.data-error');
-
-// const REMOVE_MESSAGE_TIMEOUT = 5000;
-
-// const showErrorMessage = () => {
-//   const errorElement = errorMessageTemplate.cloneNode(true);
-//   document.body.append(errorElement);
-
-//   setTimeout(() => {
-//     errorElement.remove();
-//   }, REMOVE_MESSAGE_TIMEOUT);
-// };
-
-// function debounce (callback, timeoutDelay = 500) {
-//   // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
-//   // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
-//   let timeoutId;
-
-//   return (...rest) => {
-//     // Перед каждым новым вызовом удаляем предыдущий таймаут,
-//     // чтобы они не накапливались
-//     clearTimeout(timeoutId);
-
-//     // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
-//     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-
-//     // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
-//     // пока действие совершается чаще, чем переданная задержка timeoutDelay
-//   };
-// }
-
-// export { isEscapeKey, showErrorMessage, debounce };
